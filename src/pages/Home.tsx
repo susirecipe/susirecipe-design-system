8fd4bc3e2df42ae129b71bfa0c2fda32c8961c56
kjwook7522 <kjwook7522@gmail.com>
2021-01-08 13:02:10 +0900

    feat: SPA 리로드시 redirect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import Introduce from 'components/another/Introduce/Introduce';
 
-const Home:React.FC = () => {
-  return <Introduce />
-}
+const queryString = require('query-string');
+
+const Home: React.FC = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  if (location.search) {
+    const query = queryString.parse(location.search);
+    history.replace(query.page);
+  }
+
+  return <Introduce />;
+};
 
 export default Home;
