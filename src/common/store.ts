cc0808433662f2e1db9f6c44917bc63c72803cbb
kjwook7522 <kjwook7522@gmail.com>
2021-01-22 15:00:22 +0900

    Fix: modal redux 삭제

--- src/common/store.ts
+++ src/common/store.ts
@@ -1,14 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import { slideReducer } from 'reducers/slidebar';
 import { dropdownReducer } from 'reducers/dropdown';
-import { modalReducer } from 'reducers/modal';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers({
   slidebar: slideReducer,
   dropdown: dropdownReducer,
-  modal: modalReducer,
 });
 
 const store = createStore(rootReducer);
